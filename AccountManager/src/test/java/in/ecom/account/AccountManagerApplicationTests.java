a8df318fe5413d38125e57f2d450a861d833e148
Author <satishkumardangi@gmail.com>
2022-02-01 23:42:30 +0530

    Committing Account Manager

---
+++ AccountManager/src/test/java/in/ecom/account/AccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ecom.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
